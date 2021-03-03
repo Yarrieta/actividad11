19025ab890bf3942489e2f5cec4194be3b4d184c
Yessika Arrieta <yess.arrieta4@gmail.com>
2021-03-03 01:31:52 -0300

    segunda carga

---
+++ Documentacion/actividad1_clase5.js
@@ -0,0 +1,15 @@
+var fiebre= false;
+var tos= false;
+var dolor_en_el_pecho= false;
+var malestar_muscular= false;
+var perdida_de_olfato= false;
+var perdida_de_gusto= true;
+
+if (fiebre || tos || dolor_en_el_pecho || malestar_muscular
+    || perdida_de_olfato || perdida_de_gusto){
+
+        console.log("Hazte el examen PCR")
+    }else{
+
+        console.log("Usted no tiene que hacerse el examen PCR")
+    }
